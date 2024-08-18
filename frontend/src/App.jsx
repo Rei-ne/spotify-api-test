d1f803601e1157ac41c05e6db0f362a388dcb4e8
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2024-08-18 07:45:31 +0000

    added link to article

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import spotifyLogo from './assets/icons/spotifyLogo.svg'
 function App() {
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen bg-black">
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-black font-GT_Flexa">
       <SpotifyNowPlaying />
+
+      <p className='text-white mt-8 text-center w-4/5 md:w-full'>Read my article <a className='underline' href='https://reine.hashnode.dev/how-to-connect-your-react-vitejs-app-to-spotifys-api' target='_blank'>here</a> to see how this works!</p>
     </div>
   )
 }
